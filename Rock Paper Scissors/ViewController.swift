2613418d510476903ea53fab7246e3cb60f53b72
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-03-11 15:24:35 -0600

    iboutlets, and storyboard design complete

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+@IBOutlet weak var computersChoice: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func whenPieceIsTapped(_ sender: UITapGestureRecognizer) {
+  
+    }
+    
 }
 
